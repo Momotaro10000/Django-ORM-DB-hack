0792e44d94627284f51fad90d540ac096c3df9d8
Momotaro10000 <nakanomei@gmail.com>
2021-05-05 14:20:03 +1000

    added magic_scripts

---
+++ magic_scripts.py
@@ -0,0 +1,54 @@
+from datacenter.models import Schoolkid,Mark,Lesson,Chastisement,Commendation
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import MultipleObjectsReturned
+import random
+
+def get_kid_account(kid_name):
+    try:
+        kid = Schoolkid.objects.filter(full_name__contains=kid_name).get()
+        return kid
+    except ObjectDoesNotExist:
+        print('Такого ученика {} в списке нет! Проверь, правильно ли написано имя.'.format(kid_name))
+        return None
+    except MultipleObjectsReturned:
+        print('Учеников с именем {} слишком много, возможно стоит указать полное имя.'.format(kid_name))
+        return None
+
+
+def fix_marks(kid_name):
+    kid = get_kid_account(kid_name)
+    if not kid:
+        return None
+    bad_marks = Mark.objects.filter(schoolkid=kid,points__in=[2, 3])
+    for mark in bad_marks:
+        mark.points = '5'
+        mark.save()
+
+
+def remove_chastisements(kid_name):
+    kid = get_kid_account(kid_name)
+    if not kid:
+        return None
+    chas = Chastisement.objects.filter(schoolkid=kid)
+    chas.delete()
+
+
+def create_commendation(kid_name,subject):
+    commendations = [ 'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем ожидалось!',  
+                      'Великолепно!', 'Прекрасно!', 'Сказано здорово – просто и ясно!', 
+                      'Очень хороший ответ!', 'Талантливо!', 'Уже существенно лучше!', 
+                      'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 
+                      'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!',
+                      'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 
+                      'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 
+                      'Ты растешь над собой!', 'Теперь у тебя точно все получится!']
+    kid = get_kid_account(kid_name)
+    if not kid:
+        return None
+    
+    try:
+        lesson = Lesson.objects.filter(year_of_study=kid.year_of_study,group_letter=kid.group_letter,subject__title=subject).order_by('date').first()
+        Commendation.objects.create(text=random.choice(commendations),created=lesson.date,schoolkid=kid,subject=lesson.subject,teacher=lesson.teacher)
+    except ObjectDoesNotExist:
+        print('Предмета {} в списке нет! Кажется, в названии предмета ошибка.'.format(subject))
+        return None
