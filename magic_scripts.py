6ba2ce995dcce9c9dc9d62676bda04102f6744e4
Momotaro10000 <nakanomei@gmail.com>
2021-05-08 23:52:35 +1000

    fixed magic_scripts

--- magic_scripts.py
+++ magic_scripts.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.exceptions import MultipleObjectsReturned
 import random
 
+
 def get_kid_account(kid_name):
     try:
         kid = Schoolkid.objects.filter(full_name__contains=kid_name).get()
@@ -11,7 +12,7 @@ def get_kid_account(kid_name):
         print('Такого ученика {} в списке нет! Проверь, правильно ли написано имя.'.format(kid_name))
         return None
     except MultipleObjectsReturned:
-        print('Учеников с именем {} слишком много, возможно стоит указать полное имя.'.format(kid_name))
+        print('Учеников с именем {} слишком много, укажи полное имя.'.format(kid_name))
         return None
 
 
@@ -34,21 +35,38 @@ def remove_chastisements(kid_name):
 
 
 def create_commendation(kid_name,subject):
-    commendations = [ 'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем ожидалось!',  
-                      'Великолепно!', 'Прекрасно!', 'Сказано здорово – просто и ясно!', 
-                      'Очень хороший ответ!', 'Талантливо!', 'Уже существенно лучше!', 
-                      'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 
-                      'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!',
-                      'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 
-                      'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 
-                      'Ты растешь над собой!', 'Теперь у тебя точно все получится!']
+    commendations = [ 
+        'Молодец!', 
+        'Отлично!', 
+        'Хорошо!', 
+        'Гораздо лучше, чем ожидалось!', 
+        'Великолепно!', 
+        'Прекрасно!', 
+        'Сказано здорово – просто и ясно!', 
+        'Очень хороший ответ!', 
+        'Талантливо!', 
+        'Уже существенно лучше!', 
+        'Потрясающе!', 
+        'Замечательно!', 
+        'Прекрасное начало!', 
+        'Так держать!', 
+        'Ты на верном пути!', 
+        'Здорово!', 
+        'Это как раз то, что нужно!', 
+        'Я тобой горжусь!', 
+        'С каждым разом у тебя получается всё лучше!', 
+        'Мы с тобой не зря поработали!', 
+        'Я вижу, как ты стараешься!', 
+        'Ты растешь над собой!', 
+        'Теперь у тебя точно все получится!', 
+    ]
     kid = get_kid_account(kid_name)
     if not kid:
         return None
-    
-    try:
-        lesson = Lesson.objects.filter(year_of_study=kid.year_of_study,group_letter=kid.group_letter,subject__title=subject).order_by('date').first()
-        Commendation.objects.create(text=random.choice(commendations),created=lesson.date,schoolkid=kid,subject=lesson.subject,teacher=lesson.teacher)
+    try: 
+        lessons = Lesson.objects.filter(year_of_study=kid.year_of_study,group_letter=kid.group_letter,subject__title=subject).order_by('date')
+        needed_lesson = lessons[0]
+        Commendation.objects.create(text=random.choice(commendations),created=needed_lesson.date,schoolkid=kid,subject=needed_lesson.subject,teacher=needed_lesson.teacher)
     except ObjectDoesNotExist:
         print('Предмета {} в списке нет! Кажется, в названии предмета ошибка.'.format(subject))
         return None
